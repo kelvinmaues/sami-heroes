acacd74045f0854f22a5da40519ee4978e21b527
Kelvin Maues <kgmdeveloper@gmail.com>
2020-06-05 19:12:24 -0300

    Added loading skeleton

--- src/components/HeroList/index.js
+++ src/components/HeroList/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Skeleton from "react-loading-skeleton";
 import { CardColumns } from "reactstrap";
 // internal components
 import HeroCard from "../HeroCard";
@@ -8,6 +9,7 @@ import { searchHeroByName } from "../../reducers_and_actions/justiceRoom";
 
 const HeroList = () => {
   const heroes = useSelector((state) => state.justiceRoom.heroes);
+  const isLoading = useSelector((state) => state.request.isLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,7 +20,11 @@ const HeroList = () => {
     ? heroes.map((heroObj) => <HeroCard key={heroObj.id} info={heroObj} />)
     : null;
 
-  return <CardColumns>{heroesCardList}</CardColumns>;
+  return (
+    <CardColumns>
+      {!isLoading ? heroesCardList : <Skeleton count={10} height={450} width={300} />}
+    </CardColumns>
+  );
 };
 
 export default HeroList;
